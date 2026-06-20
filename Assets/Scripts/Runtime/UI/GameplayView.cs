3800c08336ffc2617598b5421f5e36509e5785d0
Edvinas Danevičius <edvinas108@gmail.com>
2026-06-21 01:05:40 +0300

    Display counters

--- Assets/Scripts/Runtime/UI/GameplayView.cs
+++ Assets/Scripts/Runtime/UI/GameplayView.cs
@@ -1,8 +1,46 @@
+using System;
 using InSun.GameCore.UI;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
 
 namespace DoubleD.VerySeriousJamGame.Runtime.UI
 {
     internal sealed class GameplayView : View
     {
+        [Header("Counters")]
+        [SerializeField]
+        private TMP_Text remainingTimeText;
+
+        [SerializeField]
+        private TMP_Text scoreText;
+
+        [Header("Events")]
+        [SerializeField]
+        private UnityEvent onRemainingTimeChanged;
+
+        private int remainingTimePrev;
+
+        public float RemainingTime
+        {
+            set
+            {
+                var span = TimeSpan.FromSeconds(value);
+
+                var remainingTimeNext = (int)span.TotalSeconds;
+                if (remainingTimeNext != remainingTimePrev)
+                {
+                    onRemainingTimeChanged.Invoke();
+                }
+
+                remainingTimeText.text = $"{span.Minutes}:{span.Seconds:00}";
+                remainingTimePrev = remainingTimeNext;
+            }
+        }
+
+        public int Score
+        {
+            set => scoreText.text = value.ToString();
+        }
     }
 }
--- Assets/Scripts/Runtime/UI/GameplayViewController.cs
+++ Assets/Scripts/Runtime/UI/GameplayViewController.cs
@@ -1,8 +1,52 @@
+using DoubleD.VerySeriousJamGame.Runtime.Gameplay;
+using InSun.GameCore;
 using InSun.GameCore.UI;
 
 namespace DoubleD.VerySeriousJamGame.Runtime.UI
 {
     internal sealed class GameplayViewController : ViewController<GameplayView>
     {
+        private GameplaySystem gameplaySystem;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            gameplaySystem = Game.GetObject<GameplaySystem>();
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+
+            if (gameplaySystem.State is GameplayState.None or GameplayState.GameOver)
+            {
+                return;
+            }
+
+            View.RemainingTime = gameplaySystem.RemainingTime;
+        }
+
+        protected override void OnViewShowEntered()
+        {
+            base.OnViewShowEntered();
+
+            Game.AddListener<ScoreChangedMessage>(OnScoreChanged);
+
+            View.RemainingTime = gameplaySystem.RemainingTime;
+            View.Score = gameplaySystem.Score;
+        }
+
+        protected override void OnViewHideEntered()
+        {
+            base.OnViewHideEntered();
+
+            Game.RemoveListener<ScoreChangedMessage>(OnScoreChanged);
+        }
+
+        private void OnScoreChanged(ScoreChangedMessage message)
+        {
+            View.Score = message.ScoreNext;
+        }
     }
 }
