748ca1d821ec232efccff825c62cd903f81b883f
Rokas <rokas108@gmail.com>
2026-06-21 02:46:39 +0300

    Crank actor

---
+++ Assets/Scripts/Runtime/Gameplay/CrankActor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace DoubleD.VerySeriousJamGame.Runtime.Gameplay
+{
+    internal sealed class CrankActor : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform handleTransform;
+
+        [SerializeField]
+        private Rigidbody crankRigidbody;
+    }
+}
