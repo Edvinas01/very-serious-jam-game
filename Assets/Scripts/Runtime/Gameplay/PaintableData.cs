acb71d3214290e90bec79ebcaf7746936103ed30
Edvinas Danevičius <edvinas108@gmail.com>
2026-06-22 20:19:15 +0300

    Add kebab

--- Assets/Scripts/Runtime/Gameplay/PaintableData.cs
+++ Assets/Scripts/Runtime/Gameplay/PaintableData.cs
@@ -36,12 +36,11 @@ namespace DoubleD.VerySeriousJamGame.Runtime.Gameplay
         private Material material;
 
         [FormerlySerializedAs("paintableScale")]
-        [Min(0)]
+        [Min(0f)]
         [SerializeField]
         private Vector3 scale = new(1f, 1f, 1f);
 
         [FormerlySerializedAs("paintableRotation")]
-        [Min(0)]
         [SerializeField]
         private Vector3 rotation = new(0f, 0f, 0f);
 
