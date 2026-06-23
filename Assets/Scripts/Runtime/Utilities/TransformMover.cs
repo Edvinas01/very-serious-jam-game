cca4d9bf5b0af5c986729a7975274bbea7725169
Edvinas Danevičius <edvinas108@gmail.com>
2026-06-23 19:57:20 +0300

    Add transform util

---
+++ Assets/Scripts/Runtime/Utilities/TransformMover.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace DoubleD.VerySeriousJamGame.Runtime.Utilities
+{
+    internal sealed class TransformMover : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform moveTarget;
+
+        private Vector3 initialWorldPosition;
+        private Vector3 targetWorldPosition;
+
+        private void Awake()
+        {
+            initialWorldPosition = transform.position;
+        }
+
+        private void FixedUpdate()
+        {
+            var newPosition = Vector3.Lerp(
+                transform.position,
+                targetWorldPosition,
+                Time.deltaTime
+            );
+
+            transform.position = newPosition;
+        }
+
+        public void MoveToTarget()
+        {
+            targetWorldPosition = moveTarget.position;
+        }
+
+        public void MoveBack()
+        {
+            targetWorldPosition = initialWorldPosition;
+        }
+    }
+}
