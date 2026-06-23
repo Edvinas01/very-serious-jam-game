01ded91542576e6522ca2419ff0464b23d9113af
Edvinas Danevičius <edvinas108@gmail.com>
2026-06-23 19:42:37 +0300

    Add paintable audio events

---
+++ Assets/Scripts/Runtime/Audio/AudioSourceExtensions.cs
@@ -0,0 +1,32 @@
+﻿using InSun.GameCore.Utilities;
+using UnityEngine;
+
+namespace DoubleD.VerySeriousJamGame.Runtime.Audio
+{
+    internal static class AudioSourceExtensions
+    {
+        public static void PlayUsing(this AudioSource source, AudioData audioData)
+        {
+            if (source == false)
+            {
+                return;
+            }
+
+            if (audioData == null)
+            {
+                return;
+            }
+
+            source.Stop();
+
+            if (audioData.Clips.TryGetRandom(out var clip))
+            {
+                source.clip = clip;
+            }
+
+            source.volume = audioData.VolumeRange.GetRandomFloat();
+            source.pitch = audioData.PitchRange.GetRandomFloat();
+            source.Play();
+        }
+    }
+}
